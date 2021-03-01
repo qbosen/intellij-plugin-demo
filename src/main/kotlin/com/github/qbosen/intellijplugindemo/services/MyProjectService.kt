7c9d47a3a68e1ca573cb75f647fd51f8a8cdaceb
GitHub Action <action@github.com>
2021-03-01 14:19:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/qbosen/intellijplugindemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/qbosen/intellijplugindemo/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.qbosen.intellijplugindemo.services
 
+import com.github.qbosen.intellijplugindemo.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
